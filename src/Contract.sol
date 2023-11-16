ee6302572a7c9a88f2bebd42170b56b1cbe6e073
jekdev1999 <jekdev1999@gmail.com>
2023-11-16 00:36:04 -0500

    worked on NFT contract, finished,

---
+++ src/Contract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "solmate/tokens/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
+    constructor(
+    ) ERC721("helloWorld", "HLW") {}
+
+    function mintTo(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        return Strings.toString(id);
+    }
+}
